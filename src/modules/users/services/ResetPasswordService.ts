4761c7edf6a96f22a29021cd23abc67adafa0d1b
RafaelJuliano <rafael.j.ferreira@hotmail.com>
2022-04-14 23:34:33 -0300

    Seção 7: Gerenciamento de Senhas dos Usuários

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,43 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { isAfter, addHours } from 'date-fns';
+import { hash } from 'bcryptjs';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository copy';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordEmailService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const userToken = await userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User Token not found.');
+    }
+
+    const user = await usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired.');
+    }
+
+    user.password = await hash(password, 8);
+
+    await usersRepository.save(user);
+  }
+}
+
+export default ResetPasswordEmailService;
