2969591fd6cb40c99eeed916a0caeff22ab2c1a4
RafaelJuliano <rafael.j.ferreira@hotmail.com>
2022-04-15 21:43:47 -0300

    Seção 15: 115/121 - AWS SES Configurado e testado

---
+++ src/config/mail/mail.ts
@@ -0,0 +1,19 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'aswses.dev@gmail.com',
+      name: 'API Vendas',
+    },
+  },
+} as IMailConfig;
